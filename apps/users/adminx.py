2557d16221d931d80f96a1e88bab880f44b9359d
ddxiao <809660773@qq.com>
2018-12-05 19:57:45 +0800

    添加 xadmin的全局配置（标题、主题、侧边栏收起）

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,9 +1,24 @@
 # encoding: utf-8
 __author__ = 'dongxiao'
 
+from xadmin import views
 import xadmin
 from .models import EmailVerifyRecord
 from .models import Banner
+
+
+# 创建xadmin的全局管理器并和view绑定
+class BaseSetting(object):
+    # 开启主体功能
+    enable_themes = True
+    use_bootswatch = True
+
+
+# xadmin全局参数配置
+class GlobalSettings(object):
+    site_title = '蓝鸽在线后台'
+    site_footer = '蓝鸽科技'
+    menu_style = 'accordion'
 
 
 # 创建EmailVerifyRecord的管理器，不继承admin，继承object
@@ -25,3 +40,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
